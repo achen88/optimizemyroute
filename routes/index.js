9dac792d06ea143ccdf1d061347af1135f7c0199
Andrew Chen <andrew.chen@addepar.com>
2018-07-22 00:15:37 -0700

    added location search, minimal routing, api env

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+var gAPIkey = process.env.gAPIkey;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'TEMP' , googleAPI: "https://maps.googleapis.com/maps/api/js?key=" + gAPIkey + "&libraries=places&callback=initAutocomplete"});
 });
 
 module.exports = router;
